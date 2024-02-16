fc5adba9f0043c9fa5a01bdc3bdf7adfa231a1f9
Norton Almeida <norton.almeida@gmail.com>
2024-02-16 20:49:57 -0300

    Initial commit - content repo.

---
+++ src/bootstrap.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+// Mount function to start up the app
+const mount = (root) => {
+  root.render(
+    <h1>Hi there!</h1>, 
+  );
+}; 
+
+// if we are in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_content-dev-root');
+  const root = createRoot(devRoot);
+  if (devRoot) {
+    mount(root);
+  }
+} 
+
+// We are running through container and we should export the mount function
+export { mount };
